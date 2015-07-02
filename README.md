538de4bc1ad2f00f5ba4a5edfb05cc05ad01766f
Michael Murray <micahel@freeassoc.net>
2015-07-02 11:09:46 -0700

    commands, example and a test for the example

--- README.md
+++ README.md
@@ -1,1 +1,107 @@
-# project_template
+# Qumulo REST API -- Sample/ Starter Project Template
+
+## Prerequisites
+
+* Python 2.7
+* Node 0.12.4 or greater
+
+## Installing the Prerequisites
+
+### Python
+
+if you're developing on a current version of Mac OSX, you should already have a 2.7 version of python.  you can check which version of python you have by opening a command promopt and typing
+
+  python -V
+
+To install Python 2.7 please visit the [Python Software Foundation
+Download Page](https://www.python.org/downloads/)  and select the most
+current version (at time of writing it is version 2.7.10)
+
+### Node
+
+On OSX yu can run
+
+  brew install node
+
+to install the latest version, or visit [Joyent/Node](https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager) and follow instructions for your platform.
+
+## Once You Have Prerequisites Installed
+### 1. Install the supporting libraries for python and javascript
+Just run the local installer shell script to install the libraries
+you'll need for python and/or javascript samples:
+
+```
+  **./install_tools**
+```
+
+
+### 2. Install the Qumulo REST API Python Wrapper library
+Currently the Qumulo REST API python library is available only by
+request -- you'll need an oauth2 token from Qumulo in order to download
+it.
+
+Once you have a token from Qumulo, update the line in the requirements.txt file in the
+installation directory with <your token> as follows:
+
+    git+https://<your token>:x-oauth-basic@github.com/Qumulo/qumulo_rest_api
+
+and then rerun
+
+```
+  pip install -r requirements.txt
+```
+
+You can verify that you have the Qumulo REST API installed by running
+the following command at a command prompt:
+```
+  pip list
+```
+You should see something like the following output:
+
+
+```
+
+astroid (1.3.6)
+
+logilab-common (1.0.1)
+
+nose (1.3.7)
+
+pip (7.1.0)
+
+pylint (1.4.4)
+
+qumulo-api (1.2.6)
+
+setuptools (17.0)
+
+six (1.9.0)
+
+wheel (0.24.0)
+
+```
+
+
+
+### 3. Check your code quality
+We support [Pylint](http://www.pylint.org/) and [JsHint](http://jshint.com/) / [EsLint](http://eslint.org/) to ensure that sample code meets coding standards such as [PEP8](https://www.python.org/dev/peps/pep-0008/) and Qumulo's own coding standards for Python and Javascript.  To check your code against the checking rules, just run
+
+```
+
+  ./runlint
+
+```
+
+
+from the directory where you downloaded this sample project.
+
+
+### 4. Create and run tests to verify that your sample works
+We support Python unit tests via [Nose](http://pythontesting.net/framework/nose/nose-introduction/) and Javascript tests using [Jasmine](http://jasmine.github.io/2.3/introduction.html).  You can run all python and javascript tests in the current directory by running the following shell script:
+
+```
+
+  ./runtests
+
+```
+
---
+++ get_stats.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+# Copyright (c) 2013 Qumulo, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+'''
+== Description:
+
+This script will demonstrate an API call against a Qumulo REST server.
+It will first obtain authentication credentials from the server, and
+then use them in the subsequent call.
+
+== Script Usage:
+
+SCRIPTNAME.py --host ip_address|hostname [options]
+
+=== Required:
+
+[-i | --ip | --host] ip|hostname    An ip address or hostname of a node in
+                                        the cluster; use 'localhost' when
+                                        running directly on the node
+
+=== Options:
+
+[-u | --user] username              Use 'username' for authentication
+                                        (defaults to 'admin')
+[-p | --passwd] password            Use 'password' for authentication
+                                        (defaults to 'admin')
+[-P | --port] number                Use 'number' for the API server port
+                                        (defaults to 8000)
+
+-h | --help                         Print out the script usage/help
+
+=== Examples:
+
+- Run the script against a remote node
+SCRIPTNAME.py --host 192.168.194.333
+
+- Run the script against the localhost
+SCRIPTNAME.py --host localhost
+
+- Run the script against a remote node as a non-admin user
+SCRIPTNAME.py --host localhost --user new_user --pass new_password
+
+'''
+
+# Import python libraries
+import getopt
+import sys
+
+# Import Qumulo REST libraries
+# Leaving in the 'magic file path' for customers who want to run these scripts
+# from cust_demo as before
+import qumulo.lib.auth
+import qumulo.lib.request
+import qumulo.rest
+
+#### Classes
+class FileStatsCommand(object):
+    ''' class wrapper for REST API cmd so that we can new them up in tests '''
+    def __init__(self, argv=None):
+        self.port = 8000
+        self.user = 'admin'
+        self.passwd = 'admin'
+        self.demo_var = None
+        self.connection = None
+        self.creds = None
+
+        if argv:
+
+            opts = {}
+            try:
+                opts, _ = getopt.getopt(argv[1:], "hi:p:P:u:V:",\
+                                        ["help", "host=", "ip=", "user=", \
+                                        "pass=", "port="])
+
+            except getopt.GetoptError, msg:
+                print msg
+                print __doc__
+
+            for opt, arg in opts:
+                if opt in ("-h", "--help"):
+                    print __doc__
+                    sys.exit(0)
+                elif opt in ("--ip", "-i", "--host"):
+                    self.host = arg
+                elif opt in ("--port", "-P"):
+                    self.port = arg
+                elif opt in ("--user", "-u"):
+                    self.user = arg
+                elif opt in ("--pass", "-p"):
+                    self.passwd = arg
+                else:
+                    print "Unrecognized option %s\n" % opt
+                    print __doc__
+                    sys.exit(1)
+
+    def login(self):
+        '''Obtain credentials from the REST server'''
+        self.connection = None
+        self.creds = None
+
+        try:
+            # Create a connection to the REST server
+            self.connection = qumulo.lib.request.Connection(\
+                                self.host, int(self.port))
+
+            # Provide username and password to retreive authentication tokens
+            # used by the credentials object
+            login_results, _ = qumulo.rest.auth.login(\
+                    self.connection, None, self.user, self.passwd)
+
+            # Create the credentials object which will be used for
+            # authenticating rest calls
+            self.creds = qumulo.lib.auth.Credentials.\
+                    from_login_response(login_results)
+        except Exception, excpt:
+            print "Error connecting to the REST server: %s" % excpt
+            print __doc__
+            sys.exit(1)
+
+
+
+
+    def get_stats(self):
+        ''' Create directory from the provided demo_var param using fs '''
+        ## Make the API call
+        try:
+            stats = qumulo.rest.fs.read_fs_stats(self.connection, self.creds)
+        except qumulo.lib.request.RequestError, excpt:
+            print "Error: %s" % excpt
+            sys.exit(1)
+
+        return stats
+
+
+### Main subroutine
+def main():
+    ''' Main entry point '''
+    command = FileStatsCommand(sys.argv)
+    command.login()
+    stats = command.get_stats()
+    print stats
+
+
+# Main
+if __name__ == '__main__':
+    main()
